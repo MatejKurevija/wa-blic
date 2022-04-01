6e54414263cab7a79d6641ac99df3a63bf1b0c6a
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-04-01 09:49:21 +0200

    moro sam ic

---
+++ home.vue
@@ -0,0 +1,60 @@
+<template>
+<div class= "">
+	<h1 style text-align="center">Prvi page</h1>
+	<v-data-table
+	:headers="headers"
+	:name="imeknjige"
+	:authors="pautor"
+	:relesed="datumizdavanja">
+	<template v-slot:item.website="{item}">
+		<a :href="item.airline.website">{{item.airline.website}}</a>
+		</template>
+	</v-data-table>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+	name:"datakomponent",
+	data(){
+		return{
+			imeknjige:"",
+			pauthor: "",
+			datumizdavanja: "",
+			headers: [
+				{text: "Ime knjige", value:"Ime_knjige"},
+				{text: "Prvi autor", value:"Prvi_autor"},
+				{text: "datum idzavanja", value:"Datum"}
+			],
+		};
+	},
+	//pracenje podataka kad korisnik promjeni trenutni page
+watch: {
+	options: {
+		handler(){
+			this.readDataFromAPI();
+		},
+	},
+	deep: true,
+},
+	methods: {
+		readDataFromAPI(){
+			this.loading = true;
+			const {page, itemsPerPage}=this.options;
+			let pagenumber = page -1;
+			axios
+				.get("https://www.anapioficeandfire.com/api/books" + itemsPerPage + "$page= "+ pagenumber
+				)
+				.then ((response)=>{
+					this.loading = false;
+					this.
+				})
+		}
+}
+};
+</script>
+
+
+<style scoped>
+</style>
